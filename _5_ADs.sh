02be9bdbb0e768b172c9c20d43395137603df128
Bang-Chieh Huang <bjhuang0802@gmail.com>
2016-07-28 23:57:24 +0800

    Delete _5_ADs.sh

--- _5_ADs.sh
+++
@@ -1,3 +0,0 @@
-data=www_etungo_com_tw
-echo '5.How many AD click per clientId?: no.AD_per_clientId.txt'
-cat $data|jq -r '"\(.[":clientId"]),\(.["Time"]),\(.uradTracker.CurrentURL)"' |grep 'UTM'|awk -F ',' '{print $1}'|sort |uniq -c|awk '{print $2,$1}' OFS="," >dic_4_ADs.txt
