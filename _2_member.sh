804a1b6d8824d42d65d38d56870d39a4c68403be
Bang-Chieh Huang <bjhuang0802@gmail.com>
2016-07-28 23:56:53 +0800

    Delete _2_member.sh

--- _2_member.sh
+++
@@ -1,3 +0,0 @@
-data=www_etungo_com_tw
-echo '2.Generate unique Member: dic_member.txt'
-cat $data |jq -r '"\(.[":clientId"]),\(.["uradTracker"].Member)"' |awk -F ',' '{if($2==1) print $1}'|sort -g|uniq  >dic_1_member.txt
