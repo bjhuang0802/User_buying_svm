8f93c5d4f2ea104813df2d55981156fe9aa70ae2
Bang-Chieh Huang <bjhuang0802@gmail.com>
2016-07-28 23:57:01 +0800

    Delete _3_sessions.sh

--- _3_sessions.sh
+++
@@ -1,3 +0,0 @@
-data=www_etungo_com_tw
-echo '3.Generate no. of session per clientId: dic_sessions.txt'
-cat session_sort.csv |awk -F ',' '{print $1}' |uniq -c |awk '{print $2,$1}' OFS="," |sort -t, -k2 -g >dic_2_sessions.txt 
